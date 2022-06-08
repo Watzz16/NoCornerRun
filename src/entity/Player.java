94c7d7fa409d43843a6837ebf4b58b05f7dc0082
Luca Mueller <64702286+LucaMueller1@users.noreply.github.com>
2022-06-08 09:40:02 +0200

    Added player walk animation

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -14,8 +14,11 @@ public class Player extends Entity {
     KeyHandler keyHandler;
 
     BufferedImage imageIdle, imageWalk1, imageWalk2, imageJump;
-    PlayerState playerState = PlayerState.IDLE;
+    PlayerState playerState = PlayerState.WALK;
     TileManager tileManager;
+
+    private int spriteAnimCounter = 0;
+    private int walkAnimSprite = 1;
 
     int lane = 0;
 
@@ -57,11 +60,15 @@ public class Player extends Entity {
             keyHandler.downKeyPressed = false;
         }
 
+        //Switch player state to render correct animation / player sprite
         if(!keyHandler.canPressUpKey || !keyHandler.canPressDownKey) {
             playerState = PlayerState.JUMP;
         } else {
-            playerState = PlayerState.IDLE;
+            playerState = PlayerState.WALK;
         }
+
+        walkAnimation();
+
     }
 
     public void draw(Graphics2D g2) {
@@ -69,11 +76,31 @@ public class Player extends Entity {
         switch(playerState) {
             case IDLE -> currentImage = imageIdle;
             case JUMP -> currentImage = imageJump;
+            case WALK -> {
+                if(walkAnimSprite == 1) {
+                    currentImage = imageWalk1;
+                } else {
+                    currentImage = imageWalk2;
+                }
+            }
         }
 
         int currentLanePositionY = tileManager.getLanes()[lane].getLaneYPosition();
 
         g2.drawImage(currentImage, x, currentLanePositionY-gamePanel.tileSize, gamePanel.tileSize, gamePanel.tileSize, null);
+    }
+
+    private void walkAnimation() {
+        if(spriteAnimCounter > 8) {
+            if(walkAnimSprite == 1) {
+                walkAnimSprite = 2;
+            } else {
+                walkAnimSprite = 1;
+            }
+            spriteAnimCounter = 0;
+        }
+
+        spriteAnimCounter++;
     }
 
 }
